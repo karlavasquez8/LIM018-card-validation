d2a06ff8146b4bab3ca1a7b75e2da1a7878a944b
karlavasquez8 <83799832+karlavasquez8@users.noreply.github.com>
2022-05-20 17:02:48 -0500

    se crea la primera pagina

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,10 @@
 const validator = {
-  // ...
+  saludar: "Hola Nina",
+  soyUnNumero: 123,
+  esnoche: true,
+  cantar: function () {
+    console.log ("jajaja")
+  },
 };
 
 export default validator;
