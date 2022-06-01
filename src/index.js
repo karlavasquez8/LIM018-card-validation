a137ae4ebd361c0a63afc54a75b23110c0bf50bb
karlavasquez8 <83799832+karlavasquez8@users.noreply.github.com>
2022-05-31 22:40:07 -0500

    feat:Validacion terminada

--- src/index.js
+++ src/index.js
@@ -7,19 +7,33 @@ function inicializar (){
     const btnShopp= document.querySelector("#btn-shopp");
     btnShopp.addEventListener("click", btnShoppClick)
 
-
-    const closeLink= document.querySelector("#btn-modal")
+    const closeLink= document.querySelector("#btn-modal");
     closeLink.addEventListener("click",modalClose)
+
+    const form = document.querySelector("#form");
+    form.addEventListener("submit", sendForm);
 }
 
 function btnShoppClick(){
     const btnAbrir= document.querySelector(".bed-sheet");
     btnAbrir.classList.add("show-bed-sheet")
 }
-function modalClose(){
+function modalClose() {
     const bedSheet= document.querySelector(".show-bed-sheet");
     bedSheet.classList.remove("show-bed-sheet")
 }
+function sendForm(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target)
 
-console.log(validator.isValid("76757"));
-console.log(validator.maskify("7675777777"));
+    const wrapMessage = document.querySelector(".wrap-message-error");
+    wrapMessage.classList.remove("show");
+
+    if ( validator.isValid(formData.get("ccar-number")) ) {
+        const modal = document.querySelector(".modal");
+        modal.innerHTML = "felicitaciones " + formData.get("name-user") + " compraste tu ticket con la tarjeta " + validator.maskify(formData.get("ccar-number"));
+    } else { 
+        wrapMessage.classList.add('show');
+        wrapMessage.querySelector('.message').textContent = 'Tu tarjeta es invalido'
+    }
+}
--- src/validator.js
+++ src/validator.js
@@ -3,10 +3,22 @@ const validator = {
   let arrayOfNumbers=carNumber.split ("");
 
   let newArray = arrayOfNumbers.map( ( value,index)=>{
-    return index %2 ===0 ? parseInt (value)*2 : parseInt (value);
-  })
+    let double = parseInt (value)*2;
+    let sumOfPairs = double
 
-  return newArray;
+      if (double >=10){
+        let convertArray= double.toString().split("");
+         sumOfPairs = (parseInt (convertArray.at(0)) + parseInt (convertArray.at(1)));
+      }
+
+    return index %2 ===0 ? sumOfPairs : parseInt (value);
+  })
+  let sumTotal = 0;
+    newArray.forEach((value )=> {
+      (sumTotal += value )
+    });
+     return (sumTotal %10 == 0)
+  
  },
  maskify:function (ccNumber){
    return ccNumber.split("").map((value,indx)=> indx < ccNumber.length-4? "#": value).join("");
