9ffca793d0a972e415d140af577b86de775c831f
karlavasquez8 <83799832+karlavasquez8@users.noreply.github.com>
2022-06-04 12:25:48 -0500

    feat:Agregue botones y funcionalidad

--- src/index.js
+++ src/index.js
@@ -8,11 +8,10 @@ function inicializar (){
     btnShopp.addEventListener("click", btnShoppClick)
 
     const closeLink= document.querySelector("#btn-modal");
-    closeLink.addEventListener("click",modalClose)
+    closeLink.addEventListener("click",modalClose);
 
     const form = document.querySelector("#form");
     form.addEventListener("submit", sendForm);
-
 
 }
 
@@ -21,8 +20,16 @@ function btnShoppClick(){
     btnAbrir.classList.add("show-bed-sheet")
 }
 function modalClose() {
-    const bedSheet= document.querySelector(".show-bed-sheet");
-    bedSheet.classList.remove("show-bed-sheet")
+    const closeLink=document.querySelector(".show-bed-sheet");
+    closeLink.classList.remove("show-bed-sheet")
+}
+
+ function refresh(event){
+     window.location.reload();
+ }
+function cleanMessage(){
+    const cleanForm=document.querySelector(".show");
+    cleanForm.classList.remove("show")
 }
 
 function sendForm(event) {
@@ -34,10 +41,20 @@ function sendForm(event) {
 
     if ( validator.isValid(formData.get("ccar-number")) ) {
         const modal = document.querySelector(".modal");
-        modal.innerHTML = "felicitaciones " + formData.get("name-user") + " compraste tu ticket con la tarjeta " + validator.maskify(formData.get("ccar-number"));
+        modal.innerHTML = `Felicitaciones ${formData.get("name-user")} compraste tu ticket con la tarjeta ${ validator.maskify(formData.get("ccar-number"))} !!
+        <div class="container-btn">
+        <button class="re-start btn-shopp-pay"> Aceptar </button>
+        </div>`;
+        const rFresh= document.querySelector(".re-start");
+        rFresh.addEventListener("click",refresh)
+       
     } else { 
-        wrapMessage.classList.add('show');
-        wrapMessage.querySelector('.message').textContent = 'Tu tarjeta es invalido'
+        wrapMessage.classList.add("show");
+        wrapMessage.querySelector(".message").textContent = "Tu tarjeta es inválida";
+        const cleanInvalid = document.querySelector(".clean");
+        cleanInvalid.addEventListener("click",cleanMessage)
+
+
     }
 
 }
